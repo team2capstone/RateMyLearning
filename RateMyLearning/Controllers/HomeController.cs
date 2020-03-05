66f175ebd5b638a63d70ddec98482729be6e7535
CatherineOlej <37804018+CatherineOlej@users.noreply.github.com>
2020-03-05 18:28:37 -0500

    added page contenet to home and reviews

--- RateMyLearning/Controllers/HomeController.cs
+++ RateMyLearning/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace RateMyLearning.Controllers
         {
             return View();
         }
+        public IActionResult Reviews()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
