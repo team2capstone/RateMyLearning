969970da760ac7323c05c7606cca8cf3cd029125
mrodrigues95 <38141653+mrodrigues95@users.noreply.github.com>
2020-03-23 11:34:47 -0400

    added folder structure and files for user account

---
+++ RateMyLearning/Pages/Account/SignIn.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RateMyLearning.Pages.Account {
+    public class SignInModel : PageModel {
+        public void OnGet() {
+
+        }
+    }
+}
