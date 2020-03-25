acfbac6afda01290b4d85124f9f2cda658b20e5b
mrodrigues95 <38141653+mrodrigues95@users.noreply.github.com>
2020-03-25 14:24:51 -0400

    users can now edit their profile

---
+++ RateMyLearning/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,20 @@
+﻿using RateMyLearning.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RateMyLearning.ViewModels {
+    public class UserProfileViewModel {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Please provide a valid email address.")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+        public long? StudentId { get; set; }
+        public long? EmployeeId { get; set; }
+    }
+}
